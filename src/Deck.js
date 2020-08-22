9ad7ea280cffd2b58b3868bc1ad7a4aaac36e814
Craig A Stueber <58899336+CraigStueber@users.noreply.github.com>
2020-08-22 14:09:38 -0400

    Add files via upload

---
+++ src/Deck.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import Card from './Card';
+import axios from "axios";
+import "./styles.css";
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck"
+class Deck extends Component {
+    constructor(props){
+        super(props);
+        this.state = {deck:null, drawn:[]}
+        this.getCard = this.getCard.bind(this);
+    }
+    async componentDidMount(){
+        let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+        this.setState({deck: deck.data})
+        console.log(deck.data)
+    }
+    async getCard(){
+        let deck_id =  this.state.deck.deck_id;
+        try{
+            let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
+            let cardRes = await axios.get(cardUrl);
+            if (!cardRes.data.success) {
+                throw new Error ("no cards remeaining")
+            }
+            
+        let card = cardRes.data.cards[0];
+        this.setState(st => ({
+            drawn: [
+                ...st.drawn,
+                {
+                    id: card.code, 
+                    image: card.image,
+                    name: `${card.suit} of ${card.value}`
+                }
+            ]
+        }))
+        } catch(err){
+        alert(err);
+    }
+        
+        
+    }
+    render() {
+        const cards = this.state.drawn.map(c=>(
+            <Card key ={c.id} name={c.name} image={c.image} />
+        ))
+        return(
+            <div>
+                <h1 className="Deck-title">Card Dealer</h1>
+                <h2 className="Deck-title">A little demo made with React and Axios pulling from an API</h2>
+                <button className="button" onClick={this.getCard}>Get Card!</button>
+                <div className="Deck-cardarea">
+                {cards}</div>
+                
+            </div>
+        )
+    }
+}
+export default Deck;
